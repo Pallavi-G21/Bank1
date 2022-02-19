29770e20f0399913994d59edba2c812cca691e03
Pallavi <rgpallavi@gmail.com>
2022-02-19 22:00:02 +0530

    postgres added

--- Bank/settings.py
+++ Bank/settings.py
@@ -94,16 +94,7 @@ DATABASES = {
    )
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'my_database',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Abhiraj@1994',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+# 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
